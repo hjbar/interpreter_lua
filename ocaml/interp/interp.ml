da3ac2db2bf3bbb58c16f76131efec6f54c18be1
hjbar <hjbar@protonmail.com>
2024-05-03 15:06:47 +0200

    basic OCaml interpreter finished

--- ocaml/interp/interp.ml
+++ ocaml/interp/interp.ml
@@ -10,8 +10,18 @@ let create_scope (names : string list) (values : value list) :
   (name, value) Hashtbl.t =
   let htbl = Hashtbl.create 16 in
 
-  List.iter2 (fun name value -> Hashtbl.replace htbl name value) names values;
+  let rec loop l1 l2 =
+    match (l1, l2) with
+    | [], _ -> ()
+    | name :: l1', [] ->
+      Hashtbl.replace htbl name Value.Nil;
+      loop l1' l2
+    | name :: l1', value :: l2' ->
+      Hashtbl.replace htbl name value;
+      loop l1' l2'
+  in
 
+  loop names values;
   htbl
 
 (* Fonctions de l'interprète, mutuellement récursives. Une fonction par
@@ -37,10 +47,15 @@ and interp_stat (env : env) (stat : stat) : unit =
     interp_stat env s1;
     interp_stat env s2
   | Assign (var, exp) -> begin
-    let value = interp_exp env exp in
     match var with
-    | Name name -> Value.set_ident env name value
-    | IndexTable (tbl, key) -> failwith "todo"
+    | Name name ->
+      let value = interp_exp env exp in
+      Value.set_ident env name value
+    | IndexTable (tbl, k) ->
+      let table = interp_exp env tbl |> Value.as_table in
+      let key = interp_exp env k |> Value.as_table_key in
+      let value = interp_exp env exp in
+      Hashtbl.replace table key value
   end
   | FunctionCall f -> interp_funcall env f |> ignore
   | WhileDoEnd (exp, stat) ->
@@ -62,7 +77,15 @@ and interp_funcall (env : env) (fc : functioncall) : value =
     in
 
     Value.Nil
-  | Closure (names, local_env, block) -> failwith "todo"
+  | Closure (params, local_env, block) ->
+    let args_evaluated = List.map (interp_exp env) args in
+    let local_scope = create_scope params args_evaluated in
+
+    let env : env =
+      { globals = local_env.globals; locals = local_scope :: local_env.locals }
+    in
+
+    interp_block env block
 
 (* Interprète une expression *)
 and interp_exp (env : env) (e : exp) : value =
@@ -73,13 +96,16 @@ and interp_exp (env : env) (e : exp) : value =
   | Integer n -> Value.Int n
   | Float f -> Value.Float f
   | LiteralString s -> Value.String s
-  | Var var -> begin
-    match var with
-    | Name name -> Value.lookup_ident env name
-    | IndexTable (tbl, key) -> failwith "todo"
-  end
+  | Var (Name name) -> Value.lookup_ident env name
+  | Var (IndexTable (tbl, k)) ->
+    let table = interp_exp env tbl |> Value.as_table in
+    let key = interp_exp env k |> Value.as_table_key in
+    Hashtbl.find_opt table key |> Option.value ~default:Value.Nil
   | FunctionCallE f -> interp_funcall env f
-  | FunctionDef fun_body -> failwith "todo"
+  | FunctionDef fun_body ->
+    let params, code = fun_body in
+    let f = Value.Closure (params, env, code) in
+    Value.Function f
   | BinOp (op, e1, e2) -> begin
     let v1 = interp_exp env e1 in
     match op with
@@ -99,6 +125,7 @@ and interp_exp (env : env) (e : exp) : value =
       | Greater -> Value.Bool (Value.le v1 v2 |> not)
       | LessEq -> Value.Bool (Value.le v1 v2)
       | GreaterEq -> Value.Bool (Value.lt v1 v2 |> not)
+      (* logical operators *)
       | _ -> assert false
     end
   end
@@ -108,7 +135,17 @@ and interp_exp (env : env) (e : exp) : value =
     | UnaryMinus -> Value.neg value
     | Not -> Value.Bool (Value.as_bool value |> not)
   end
-  | Table items -> failwith "todo"
+  | Table items ->
+    let htbl = Hashtbl.create 16 in
+
+    List.iter
+      (fun (key, value) ->
+        let k = interp_exp env key |> Value.as_table_key in
+        let v = interp_exp env value in
+        Hashtbl.replace htbl k v )
+      items;
+
+    Value.Table htbl
 
 let run ast =
   let globals = Hashtbl.create 47 in
